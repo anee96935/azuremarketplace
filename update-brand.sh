da01f1fc0f77cbfcc139844d5f7790e640c3aa83
anee96935 <anee96935@gmail.com>
2025-07-18 18:52:46 +0530

    update the azuredeployee and update brand file 18-july-v4

--- update-brand.sh
+++ update-brand.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-DB_USER="$1"
-DB_PASS="$2"
-DB_NAME="$3"
-BRAND_NAME="$4"
+BRAND_NAME="$1"
+ADMIN_USER="$2"
+ADMIN_PASS="$3"
 
-echo "Starting brand update for $BRAND_NAME"
+# Read DB credentials from a secure local file or env vars
+DB_USER=$(cat /etc/athena/db_user)
+DB_PASS=$(cat /etc/athena/db_pass)
+DB_NAME=$(cat /etc/athena/db_name)
 
-# Update site title and description
-mysql -u "$DB_USER" -p"$DB_PASS" -e "
-USE $DB_NAME;
+NEW_IP=$(curl -s -H Metadata:true "http://169.254.169.254/metadata/instance/network/interface/0/ipv4/ipAddress/0/publicIpAddress?api-version=2021-02-01&format=text")
+
+mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" -e "
 UPDATE wp_options SET option_value = '$BRAND_NAME' WHERE option_name IN ('blogname', 'blogdescription');
-UPDATE wp_options SET option_value = REPLACE(option_value, 'http://OLD_IP', 'http://NEW_IP') WHERE option_name IN ('siteurl', 'home');
+UPDATE wp_options SET option_value = 'http://$NEW_IP' WHERE option_name IN ('siteurl', 'home');
+UPDATE wp_users SET user_login = '$ADMIN_USER', user_pass = MD5('$ADMIN_PASS') WHERE ID = 1;
 "
 
-# Update plugin branding if needed
 sed -i "s/page=athena/page=$BRAND_NAME/g" /var/www/html/wp-content/plugins/athena/lib/helpers/athena_grid_helper.php
-
-echo "Brand update completed" >> /var/log/brand-update.log
